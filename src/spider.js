f085bdb9106b486bdab09577a3708b217add79cc
Minhaz Kazi <vlookup@google.com>
2026-06-04 16:50:50 +0000

    Fix OSPO compliance issues

--- src/spider.js
+++ src/spider.js
@@ -1,3 +1,8 @@
+// © 2019 Google LLC.  All rights reserved.
+//
+// This software is subject to the Google Cloud Terms of Service, as
+// modified by the "General Software Terms" of the Google Cloud Service Specific Terms, available at: https://cloud.google.com/terms/service-terms.
+
 /**
  * Welcome to the Looker Visualization Builder! Please refer to the following resources
  * to help you write your visualization:
